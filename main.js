6f1e23dc14336431409017bec4ef82856689d2c5
Yikuan Sun <yikuansun2015@gmail.com>
2021-01-18 13:45:40 -0500

    fixed iohook?

--- main.js
+++ main.js
@@ -24,8 +24,10 @@ app.whenReady().then(() => {
   app.on('activate', function () {
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
   })
-})
+});
 
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') app.quit();
-})
+});
+
+app.allowRendererProcessReuse = false;
